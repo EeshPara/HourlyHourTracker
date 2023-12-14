d961619dcc2144c29ddb863e2e38958da026c43b
Laksh Gulati <gulatilaksh@outlook.com>
2023-12-13 19:15:43 -0600

    added viewaccountpage

---
+++ HourlyHourTracker/View/Other/ViewAccountPage.swift
@@ -0,0 +1,18 @@
+//
+//  ViewAccountPage.swift
+//  HourlyHourTracker
+//
+//  Created by Laksh Gulati on 12/13/23.
+//
+
+import SwiftUI
+
+struct ViewAccountPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ViewAccountPage()
+}
